0e84ecdfc6c464607751424393a5d38977120896
Doan Minh Phuc <phucdm1901@gmail.com>
2016-11-17 21:53:23 +0700

    new Student

---
+++ PhpProject/admin/addnewStudent.php
@@ -0,0 +1,48 @@
+<?php require "../header.php"; ?>
+
+<div class = "group-box">
+    <div align="center"> 
+        <div class="title">THÊM SINH VIÊN</div>
+        <div class="group-box-content">
+            <table class="formInfo"">
+                <tr>
+                    <th>Mã số sinh viên:</th>
+                    <th><input type="text"/></th>
+                </tr>
+                <tr>
+                    <th>Họ và tên sinh viên:</th>
+                    <th><input type="text"/></th>
+                </tr>
+                <tr>
+                    <th>Ngày sinh:</th>
+                    <th><input type="text"/></th>
+                </tr>
+                <tr>
+                    <th>Giới tính:</th>
+                    <th><input type="text"/></th>
+                </tr>
+                <tr>
+                    <th>Quê Quán:</th>
+                    <th><input type="text"/></th>
+                </tr>
+                <tr>
+                    <th>Khóa học:</th>
+                    <th><input type="text"/></th>
+                </tr>
+                <tr>
+                    <th>Khoa/viện:</th>
+                    <th><input type="text"/></th>
+                </tr>
+                <tr>
+                    <th>Lớp:</th>
+                    <th><input type="text"/></th>
+                </tr>
+                <tr>
+                    <th>Hệ:</th>
+                    <th><input type="text"/></th>
+                </tr>
+            </table>
+        </div>
+    </div>
+</div>
+<?php require "../footer.php"?>
