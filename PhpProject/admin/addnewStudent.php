ae30da3d11393ff12b798c8231817cd027aba8db
Doan Minh Phuc <phucdm1901@gmail.com>
2016-11-18 10:13:11 +0700

    add new Student

--- PhpProject/admin/addnewStudent.php
+++ PhpProject/admin/addnewStudent.php
@@ -4,44 +4,51 @@
     <div align="center"> 
         <div class="title">THÊM SINH VIÊN</div>
         <div class="group-box-content">
-            <table class="formInfo"">
-                <tr>
-                    <th>Mã số sinh viên:</th>
-                    <th><input type="text"/></th>
-                </tr>
-                <tr>
-                    <th>Họ và tên sinh viên:</th>
-                    <th><input type="text"/></th>
-                </tr>
-                <tr>
-                    <th>Ngày sinh:</th>
-                    <th><input type="text"/></th>
-                </tr>
-                <tr>
-                    <th>Giới tính:</th>
-                    <th><input type="text"/></th>
-                </tr>
-                <tr>
-                    <th>Quê Quán:</th>
-                    <th><input type="text"/></th>
-                </tr>
-                <tr>
-                    <th>Khóa học:</th>
-                    <th><input type="text"/></th>
-                </tr>
-                <tr>
-                    <th>Khoa/viện:</th>
-                    <th><input type="text"/></th>
-                </tr>
-                <tr>
-                    <th>Lớp:</th>
-                    <th><input type="text"/></th>
-                </tr>
-                <tr>
-                    <th>Hệ:</th>
-                    <th><input type="text"/></th>
-                </tr>
-            </table>
+            <form>
+                <table class="formInfo"">
+                    <tr>
+                        <th id="titleform">Mã số sinh viên:</th>
+                        <th><input type="text" size="40"/></th>
+                    </tr>
+                    <tr>
+                        <th id="titleform">Họ và tên sinh viên:</th>
+                        <th><input type="text" size="40"/></th>
+                    </tr>
+                    <tr>
+                        <th id="titleform">Ngày sinh:</th>
+                        <th><input type="text" size="40"/></th>
+                    </tr>
+                    <tr>
+                        <th id="titleform">Giới tính:</th>
+                        <th><input type="text" size="40"/></th>
+                    </tr>
+                    <tr>
+                        <th id="titleform">Quê Quán:</th>
+                        <th><input type="text" size="40"/></th>
+                    </tr>
+                    <tr>
+                        <th id="titleform">Khóa học:</th>
+                        <th><input type="text" size="40"/></th>
+                    </tr>
+                    <tr>
+                        <th id="titleform">Khoa/viện:</th>
+                        <th><input type="text" size="40"/></th>
+                    </tr>
+                    <tr>
+                        <th id="titleform">Lớp:</th>
+                        <th><input type="text" size="40"/></th>
+                    </tr>
+                    <tr>
+                        <th id="titleform">Hệ:</th>
+                        <th><input type="text" size="40"/></th>
+                    </tr>
+                    <tr></tr>
+                    <tr>
+                        <th></th>
+                        <th><input type="submit" value="Thêm sinh viên"/></th>
+                    </tr>
+                </table>
+            </form>
         </div>
     </div>
 </div>
