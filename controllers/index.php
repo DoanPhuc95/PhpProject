5e90d321970b3d3de2a9918340079f8e18ef3099
Nguyen Khanh Hung <khung9895@gmail.com>
2016-11-18 14:19:53 +0700

    MVC from scratch

---
+++ controllers/index.php
@@ -0,0 +1,12 @@
+
+<? php
+require_once "libs/Controller.php";
+
+class Index extends Controller
+{
+  public function index()
+  {
+    echo 'Hello World from index controller';
+  }
+}
+?>
---
+++ libs/View.php
@@ -0,0 +1,19 @@
+<? php
+
+class View
+{
+
+  public function __construct()
+  {
+
+  }
+
+  public function render($viewScript)
+  {
+     require($viewScript);
+  }
+
+}
+
+
+ ?>
